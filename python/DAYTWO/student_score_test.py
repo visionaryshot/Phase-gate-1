628ecebeb38c3e166341b3e4f03615c340d5e4d6
Adedeji Fareed <adedejiphareed22@gmail.com>
2025-08-19 16:38:20 +0100

    Add files via upload

---
+++ python/DAYTWO/student_score_test.py
@@ -0,0 +1,16 @@
+import unittest
+import Tasktwo
+
+ClassTeststudent_score_testFunction(unittest.Test.Case):
+def test_that_student_score_test_Function_return_correct_result(self):
+	result = student_score_test.get_student_score(score1, score2, score3)
+	self.assertEqual(result,sum)
+
+
+def test_that_student_score_test_Function_raise_validation_with_incorrect(self):
+	self.assertRaise(valueError,student_score_test.get_student_score,"Fareed")
+
+
+def test_that_student_score_test_Function_return_correct_result(self):
+	result = student_score_test.get_student_score(score1, score2, score3)
+	self.assertNotEqual(result,product) 
